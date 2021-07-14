6ef1a5c38277c2053e5e31d1d888f6c1048c5829
zxy-lgtm <1563925535@qq.com>
2021-07-14 16:14:54 +0800

    update a new option

--- 1.go
+++ 1.go
@@ -1,13 +1,26 @@
 func twoSum(nums []int, target int) []int {
-    r := make([]int,2)
-    for i := 0;i < len(nums) - 1; i++{
-        for j := i+1; j < len(nums); j ++{
-            if(nums[i]+nums[j] == target){
-                r[0] = i
-                r[1] = j
-                return r
-            }
-        }
-   }
-   return r
+	r := make([]int, 2)
+	for i := 0; i < len(nums)-1; i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				r[0] = i
+				r[1] = j
+				return r
+			}
+		}
+	}
+	return r
+}
+
+//更优解，使用map
+func twoSum(nums []int, target int) []int {
+	m := make(map[int]int)
+	for index, val := range nums {
+		if preIndex, ok := m[target-val]; ok {
+			return []int{preIndex, index}
+		} else {
+			m[val] = index
+		}
+	}
+	return []int{}
 }
