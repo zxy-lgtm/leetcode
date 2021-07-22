c175b9d49827599a66ba41a7d989cd52662f1687
Lemonade <71624441+zxy-lgtm@users.noreply.github.com>
2021-07-22 18:39:30 +0800

    3

--- 3.go
+++ 3.go
@@ -5,6 +5,32 @@ import (
 )
 
 func lengthOfLongestSubstring(s string) int {
+	cnt := len(s)
+	if cnt == 0 {
+		return 0
+	}
+
+	tempMap := make(map[uint8]int)
+	l, r, maxLen := 0, 1, 1
+	tempMap[s[l]] = l
+	for l < r && r < cnt {
+		val, ok := tempMap[s[r]]
+		if ok && val >= l {
+			l = val + 1
+		}
+
+		if r-l+1 > maxLen {
+			maxLen = r - l + 1
+		}
+
+		tempMap[s[r]] = r
+		r++
+	}
+
+	return maxLen
+}
+
+func lengthOfLongestSubstring(s string) int {
 	start, end := 0, 0
 	for i := 0; i < len(s); i++ {
 		index := strings.Index(s[start:i], string(s[i]))
