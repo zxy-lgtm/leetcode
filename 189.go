f93c85a0dd22db37afea40805f5063cf29587d2c
zxy-lgtm <1563925535@qq.com>
2021-07-26 12:46:15 +0800

    189-other options

--- 189.go
+++ 189.go
@@ -23,6 +23,40 @@ func rotate(nums []int, k int) {
 
 }
 
+//环状替换
+func rotate2(nums []int, k int) {
+	n := len(nums)
+	k %= n
+	for start, count := 0, gcd(k, n); start < count; start++ {
+		pre, cur := nums[start], start
+		for ok := true; ok; ok = cur != start {
+			next := (cur + k) % n
+			nums[next], pre, cur = pre, nums[next], next
+		}
+	}
+}
+
+func gcd(a, b int) int {
+	for a != 0 {
+		a, b = b%a, a
+	}
+	return b
+}
+
+//翻转数组
+func reverse(a []int) {
+	for i, n := 0, len(a); i < n/2; i++ {
+		a[i], a[n-1-i] = a[n-1-i], a[i]
+	}
+}
+
+func rotate3(nums []int, k int) {
+	k %= len(nums)
+	reverse(nums)
+	reverse(nums[:k])
+	reverse(nums[k:])
+}
+
 /*
 func main() {
 	nums := []int{1, 2, 3}
