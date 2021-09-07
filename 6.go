962370e8568d27ea835ba9bf714bff9379fefaf1
zxy-lgtm <1563925535@qq.com>
2021-09-07 14:34:09 +0800

    6

---
+++ 6.go
@@ -0,0 +1,16 @@
+func convert(s string, numRows int) string {
+	if numRows == 1 {
+		return s
+	}
+	re := make([]string, numRows)
+	n := 2*numRows - 2
+	for i, char := range s {
+		x := i % n
+		if (n - x) > x {
+			re[x] += string(char)
+		} else {
+			re[n-x] += string(char)
+		}
+	}
+	return strings.Join(re, "")
+}
