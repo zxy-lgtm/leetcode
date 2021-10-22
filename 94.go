97704042edc1f568b50a8ab37b04c0dedf3125ab
zxy-lgtm <1563925535@qq.com>
2021-10-22 23:21:17 +0800

    stack

--- 94.go
+++ 94.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "container/list"
+
 //Definition for a binary tree node.
 type TreeNode struct {
 	Val   int
@@ -25,6 +27,39 @@ func inorderTraversal(root *TreeNode) (res []int) {
 	}
 
 	i(root)
+
+	return
+}
+
+func inorderTraversal_1(root *TreeNode) (res []int) {
+
+	if root == nil {
+		return nil
+	}
+
+	stack := list.New()
+	node := root
+	//先将所有左节点找到，加入栈中
+	for node != nil {
+		stack.PushBack(node)
+		node = node.Left
+	}
+	//其次对栈中的每个节点先弹出加入到结果集中，再找到该节点的右节点的所有左节点加入栈中
+	for stack.Len() > 0 {
+
+		e := stack.Back()
+		node := e.Value.(*TreeNode)
+		stack.Remove(e)
+		//找到该节点的右节点，再搜索他的所有左节点加入栈中
+		res = append(res, node.Val)
+		node = node.Right
+
+		for node != nil {
+			stack.PushBack(node)
+			node = node.Left
+		}
+
+	}
 
 	return
 }
