0162f5244772f06cbabd011ad1a194ed4f895d41
zxy-lgtm <1563925535@qq.com>
2021-10-22 23:17:54 +0800

    tree by stack

--- 144.go
+++ 144.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "container/list"
+
 //Definition for a binary tree node.
 type TreeNode struct {
 	Val   int
@@ -25,4 +27,36 @@ func preorderTraversal(root *TreeNode) (res []int) {
 	r(root)
 
 	return
+}
+
+func preorderTraversal_1(root *TreeNode) (res []int) {
+
+	if root == nil {
+		return nil
+	}
+
+	var stack = list.New()
+
+	stack.PushBack(root.Right)
+	stack.PushBack(root.Left)
+	res = append(res, root.Val)
+
+	for stack.Len() > 0 {
+
+		e := stack.Back()
+		stack.Remove(e)
+		node := e.Value.(*TreeNode) //e是Element类型，其值为e.Value.由于Value为接口，所以要断言
+
+		if node == nil {
+			continue
+		}
+
+		res = append(res, node.Val)
+		stack.PushBack(node.Right)
+		stack.PushBack(node.Left)
+
+	}
+
+	return res
+
 }
--- 145.go
+++ 145.go
@@ -1,5 +1,7 @@
 package leetcode
 
+import "container/list"
+
 //Definition for a binary tree node.
 type TreeNode struct {
 	Val   int
@@ -27,4 +29,39 @@ func postorderTraversal(root *TreeNode) (res []int) {
 	p(root)
 
 	return
+}
+
+func postorderTraversal_1(root *TreeNode) (res []int) {
+
+	if root == nil {
+		return nil
+	}
+
+	var stack = list.New()
+
+	stack.PushBack(root.Left)
+	stack.PushBack(root.Right)
+	res = append(res, root.Val)
+
+	for stack.Len() > 0 {
+
+		e := stack.Back()
+		stack.Remove(e)
+		node := e.Value.(*TreeNode) //e是Element类型，其值为e.Value.由于Value为接口，所以要断言
+
+		if node == nil {
+			continue
+		}
+
+		res = append(res, node.Val)
+		stack.PushBack(node.Left)
+		stack.PushBack(node.Right)
+
+	}
+
+	for i := 0; i < len(res)/2; i++ {
+		res[i], res[len(res)-i-1] = res[len(res)-i-1], res[i]
+	}
+
+	return res
 }
