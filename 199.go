83ec23ea24191c7bbf1dc09422310e414cf49887
zxy-lgtm <1563925535@qq.com>
2021-10-23 19:38:25 +0800

    a new way

--- 199.go
+++ 199.go
@@ -40,3 +40,30 @@ func rightSideView(root *TreeNode) (ans []int) {
 	}
 	return
 }
+
+// 减少内存的方法
+func rightSideView_1(root *TreeNode) (ans []int) {
+	if root == nil {
+		return
+	}
+
+	queue := list.New()
+	queue.PushBack(root)
+
+	for queue.Len() != 0 {
+		len := queue.Len()
+		for i := 0; i < len; i++ {
+			node := queue.Remove(queue.Front()).(*TreeNode)
+			if node.Left != nil {
+				queue.PushBack(node.Left)
+			}
+			if node.Right != nil {
+				queue.PushBack(node.Right)
+			}
+			if i == len-1 {
+				ans = append(ans, node.Val)
+			}
+		}
+	}
+	return
+}
