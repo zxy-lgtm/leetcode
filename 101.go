e7f691ee59be37603d55d8d5f670eaa2a269730f
zxy-lgtm <1563925535@qq.com>
2021-10-24 12:24:11 +0800

    is symmetric

--- 101.go
+++ 101.go
@@ -28,3 +28,24 @@ func isSymmetric(root *TreeNode) bool {
 
 	return compare(root.Left, root.Right)
 }
+
+// 迭代
+func isSymmetric_1(root *TreeNode) bool {
+	var queue []*TreeNode
+	if root != nil {
+		queue = append(queue, root.Left, root.Right)
+	}
+	for len(queue) > 0 {
+		left := queue[0]
+		right := queue[1]
+		queue = queue[2:]
+		if left == nil && right == nil {
+			continue
+		}
+		if left == nil || right == nil || left.Val != right.Val {
+			return false
+		}
+		queue = append(queue, left.Left, right.Right, right.Left, left.Right)
+	}
+	return true
+}
