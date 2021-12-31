31cdc2ceaecb7fe63b5bc6f6d20c9bae6c825416
zxy-lgtm <1563925535@qq.com>
2021-12-31 08:40:04 +0800

    yanghuisanjiao

---
+++ exercise/119.go
@@ -0,0 +1,22 @@
+package exercise
+
+func getRow(rowIndex int) []int {
+	row := make([]int, rowIndex+1)
+	row[0] = 1
+	for i := 1; i <= rowIndex; i++ {
+		for j := i; j > 0; j-- {
+			row[j] += row[j-1]
+		}
+	}
+	return row
+}
+
+// O(rowIndex)的时间复杂度
+func getRow_(rowIndex int) []int {
+	row := make([]int, rowIndex+1)
+	row[0] = 1
+	for i := 1; i <= rowIndex; i++ {
+		row[i] = row[i-1] * (rowIndex - i + 1) / i
+	}
+	return row
+}
