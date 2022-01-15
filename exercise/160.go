1db85dfe2cc2f08297bd9c3cd48feee3f78457d2
zxy-lgtm <1563925535@qq.com>
2022-01-15 21:22:49 +0800

    leetcode data-struct day11

---
+++ exercise/160.go
@@ -0,0 +1,19 @@
+package exercise
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	vis := map[*ListNode]bool{}
+	for tmp := headA; tmp != nil; tmp = tmp.Next {
+		vis[tmp] = true
+	}
+	for tmp := headB; tmp != nil; tmp = tmp.Next {
+		if vis[tmp] {
+			return tmp
+		}
+	}
+	return nil
+}
